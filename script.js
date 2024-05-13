882133fbe3e83c2eb6283538d816d76dee70fbb6
rafialfd <134205268+rafialfd@users.noreply.github.com>
2024-05-13 19:18:57 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const siSpan = document.querySelector(".picit");
-const siBar = document.querySelector(".navbar");
-
-siSpan.addEventListener("click", () => {
-  siSpan.classList.toggle("active");
-  siBar.classList.toggle("active");
-});
